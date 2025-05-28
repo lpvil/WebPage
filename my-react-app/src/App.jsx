392872f71b1298fb0ee7a720b2493e83fb5d7b0a
lpvil <lpvillalone@gmail.com>
2025-05-27 21:58:51 -0700

    image updates

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -2,8 +2,8 @@ import Card from "./Card.jsx"
 import Button from "./Button.jsx";
 import Profile from "./Profiles.jsx"
 import vic from "./assets/victorhugo.jpg"
-import lo from "./assets/lo.jpg"
-import gord from "./assets/gordon.jpeg"
+import lo from "./assets/leandro.jpg"
+import gord from "./assets/gord.jpg"
 
 function App() {
   
@@ -12,7 +12,7 @@ function App() {
     <Card/>
     <Profile profilePic={vic} name="Victor Hugo" descrpt="Four-time IBJJF World champion at black belt. Earned his black belt form Saulo and Xande Ribeiro at Six Blades Association" />
     <Profile profilePic={lo} name="Leandro Lo" descrpt="Eight-time IBJJF World Champion in five different weight classes. Earned black belt under Cicero Costha"/>
-    <Profile profilePic={gord} name="Gordon Ryan" descrpt="Seven-time ADCC World Champion "/>
+    <Profile profilePic={gord} name="Gordon Ryan" descrpt="Seven-time ADCC World Champion. Earned his black belt under Gary Tonon. "/>
     </>
   
   
