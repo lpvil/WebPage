c4b8ce09faed2030fadc8d68a4e19e3fe4bf2286
lpvil <lpvillalone@gmai.com>
2025-05-28 13:51:44 -0700

    added profiles

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -3,6 +3,8 @@ import Profile from "./Profiles.jsx"
 import vic from "./assets/victorhugo.jpg"
 import lo from "./assets/leandro.jpg"
 import gord from "./assets/gord.jpg"
+import creg from "./assets/creg.webp"
+import giles from "./assets/giles.png"
 
 function App() {
   
@@ -12,6 +14,8 @@ function App() {
     <Profile profilePic={vic} name="Victor Hugo" descrpt="Four-time IBJJF World champion at black belt. Earned his black belt form Saulo and Xande Ribeiro at Six Blades Association" />
     <Profile profilePic={lo} name="Leandro Lo" descrpt="Eight-time IBJJF World Champion in five different weight classes. Earned black belt under Cicero Costha"/>
     <Profile profilePic={gord} name="Gordon Ryan" descrpt="Seven-time ADCC World Champion. Earned his black belt under Gary Tonon. "/>
+    <Profile profilePic={creg} name="Craig Jones" descrpt="Second Best" />
+    <Profile profilePic={giles} name="Lachlan Giles" descrpt="2019 ADCC bronze medalist 2017 World No-Gi Championship Bronze Medalist.Earned his black belt under John Simon." />
     </>
   
   
