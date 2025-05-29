17c01bcd89c5cde40630b283da075593d7ae6042
lpvil <lpvillalone@gmail.com>
2025-05-28 22:45:03 -0700

    nav/home page updates

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -5,12 +5,13 @@ import lo from "./assets/leandro.jpg"
 import gord from "./assets/gord.jpg"
 import creg from "./assets/creg.webp"
 import giles from "./assets/giles.png"
+import me from "./assets/me.jpg"
 
 function App() {
   
   return(    
     <>
-    <Card/>
+    <Profile profilePic={me} name="Jesus Lopez" descrpt="Hi, I'm Jesus Welcome to my Page"/>
     <Profile profilePic={vic} name="Victor Hugo" descrpt="Four-time IBJJF World champion at black belt. Earned his black belt form Saulo and Xande Ribeiro at Six Blades Association" />
     <Profile profilePic={lo} name="Leandro Lo" descrpt="Eight-time IBJJF World Champion in five different weight classes. Earned black belt under Cicero Costha"/>
     <Profile profilePic={gord} name="Gordon Ryan" descrpt="Seven-time ADCC World Champion. Earned his black belt under Gary Tonon. "/>
