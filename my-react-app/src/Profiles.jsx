fc9fb7d6598c2ce44d81978d43d6c57b3449ff1e
lpvil <lpvillalone@gmai.com>
2025-05-28 20:09:12 -0700

    nav

--- my-react-app/src/Profiles.jsx
+++ my-react-app/src/Profiles.jsx
@@ -6,6 +6,7 @@ function Profile(props){
             <img src={props.profilePic} className="profpic" alt="profile picture"></img>
             <h2 className="card-title">{props.name}</h2>
             <p className="card-text">{props.descrpt}</p>
+            <nav className="nav"><a className="link"href={props.links}></a></nav>
         </div>
         
     );
